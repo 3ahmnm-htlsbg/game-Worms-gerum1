373e3a4cbfdbecffb0fa29d10e687d315168540f
“gerum1” <richard.gerum02@htl-salzburg.ac.at>
2020-01-21 15:06:09 +0100

    create Script, Cubes, and Workspace

---
+++ Assets/MyGame/Scripts/WormController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    public int x;
+    public string text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (x>0)
+        {
+            Debug.Log("x ist größer als 0");
+        }
+        else
+        {
+            Debug.Log("x ist kleiner als 0");
+        }
+
+        if (text == "Richie")
+        {
+            Debug.Log("Cool");
+        }
+        else
+        {
+            Debug.Log("Nicht so cool");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
