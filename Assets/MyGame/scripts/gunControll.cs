2b6de34a9e4f26bd2f5057129a95d1adc5de08b1
“gerum1” <richard.gerum02@htl-salzburg.ac.at>
2020-03-03 13:44:58 +0100

    make Scripts and make the game run

---
+++ Assets/MyGame/scripts/gunControll.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunControll : MonoBehaviour
+{
+    private Rigidbody rb;
+    public GameObject gunCubeSpawn;
+    private Vector3 gunCubeSpawnPos;
+
+    private Quaternion gunCubeSpawnRot;
+    public GameObject bullet;
+    private GameObject bulletInst;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+            if (Input.GetKeyDown("2"))
+            {
+                    shoot();
+            }
+          
+    }
+    
+    void shoot()
+    {
+        Rigidbody rbBullet;
+        //get GunCube Pos and Rotation
+        gunCubeSpawnPos = gunCubeSpawn.transform.position;
+        gunCubeSpawnRot = gunCubeSpawn.transform.rotation;
+        //Instatiate
+        bulletInst = Instantiate(bullet, gunCubeSpawnPos, gunCubeSpawnRot) as GameObject;
+        //add force
+        rbBullet = bulletInst.GetComponent<Rigidbody>();
+        Debug.Log(rbBullet);
+        rbBullet.AddForce(this.transform.up * -2f, ForceMode.Impulse);
+    }
+}
---
+++ Assets/MyGame/scripts/playerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+            if (Input.GetKey("a"))
+            {
+                rb.AddForce(-.5f, 0f, 0f, ForceMode.Impulse);
+            }
+            if (Input.GetKey("d"))
+            {
+                rb.AddForce(.5f, 0f, 0f, ForceMode.Impulse);
+            }
+            if (Input.GetKeyDown("s"))
+            {
+                jump();
+            }
+        }
+    
+    void jump()
+    {
+        rb.AddForce(0f, 5f, 0f, ForceMode.Impulse);
+    }
+}
